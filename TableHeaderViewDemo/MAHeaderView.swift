cb5f173290d62a8fc091e1e728b49cd32d4a1b2e
fengyunjue <ma772528138@qq.com>
2021-04-27 14:57:33 +0800

    first commit

---
+++ TableHeaderViewDemo/MAHeaderView.swift
@@ -0,0 +1,73 @@
+//
+//  MAHeaderView.swift
+//  TableHeaderViewDemo
+//
+//  Created by admin on 4/27/21.
+//
+
+import UIKit
+import MAAutoLayout
+
+class MAHeaderView: UIView {
+
+    public var title: String = "" {
+        didSet {
+            self.titleLabel.text = title
+        }
+    }
+    
+    private lazy var headerView: UIView = {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.red
+        headerView.layer.cornerRadius = 45
+        return headerView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.numberOfLines = 0
+        return label
+    }()
+    private lazy var lineView: UIView = {
+        let lineView = UIView()
+        lineView.backgroundColor = UIColor.black
+        return lineView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addSubview(self.headerView)
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.lineView)
+        
+        self.headerView.ma_makeConstraints { (make) in
+            make.topLeft.equalTo(self).offset(15)
+            make.size.ma_equal(90)
+            make.bottom.lessThanOrEqualTo(self).offset(-15)
+        }
+        self.titleLabel.ma_makeConstraints { (make) in
+            make.top.equalTo(self.headerView)
+            make.left.equalTo(self.headerView.ma_right).offset(15)
+            make.right.equalTo(self).offset(-15)
+            make.bottom.equalTo(self).offset(-15).priority(.defaultHigh)
+        }
+        self.titleLabel.setContentHuggingPriority(UILayoutPriority.init(1000), for: .vertical)
+        self.lineView.ma_makeConstraints { (make) in
+            make.bottomLeftRight.equalTo(self)
+            make.height.ma_equal(0.5)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.titleLabel.preferredMaxLayoutWidth = self.frame.size.width - 15 - 90 - 15 - 15
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ TableHeaderViewDemo/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  TableHeaderViewDemo
+//
+//  Created by admin on 4/27/21.
+//
+
+import UIKit
+import MAAutoLayout
+
+class ViewController: UIViewController {
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView.init(frame: CGRect.zero, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.tableHeaderView = MAHeaderView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.size.width, height: 120))
+        tableView.rowHeight = 44
+        return tableView
+    }()
+    
+    var string: String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(self.tableView)
+        self.tableView.ma_makeConstraints { (make) in
+            make.edge.equalTo(self.view)
+        }
+        self.refreshAction(self.navigationItem.rightBarButtonItem!)
+    }
+
+    @IBAction func refreshAction(_ sender: UIBarButtonItem) {
+        self.string += Date.init().description
+        let headerView = tableView.tableHeaderView as! MAHeaderView // 获取headerView
+        headerView.title = self.string
+        
+        let smallSize =  headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)  //获取到基于Autolayout布局控件尽可能小的大小
+        var tempFrame = headerView.frame
+        tempFrame.size.height = smallSize.height
+        headerView.frame = tempFrame           //  直接更改frame无效
+        tableView.tableHeaderView = headerView // 【重点】重新为 TableView 赋值 Header
+        self.tableView.reloadData()
+    }
+    
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = "\(Date().description)-\(indexPath.row)"
+        return cell
+    }
+    
+}
